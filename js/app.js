99e21c09d9fd75ccf6f359601298115fbfc7af17
AyalaAx3l <sciencetech.mefes.2021@gmail.com>
2024-05-08 22:59:02 -0600

    11v = 134

---
+++ js/app.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    // Seleccionar los elementos de la interfaz
+    const inputEmail = document.querySelector('#email')
+    const inputAsunto = document.querySelector('#asunto')
+    const inputMensaje = document.querySelector('#mensaje')
+
+    const formulario = document.querySelector('#formulario')
+    
+    // Asignar eventos
+    inputEmail.addEventListener('blur',validar);
+    inputAsunto.addEventListener('blur',validar);
+    inputMensaje.addEventListener('blur',validar);   
+
+    function validar(e){        
+        if(e.target.value.trim() === ''){
+            mostrarAlerta(`El Campo ${e.target.id} es obligatorio`, e.target.parentElement);
+        }
+        else{
+            console.log('si hay algo');
+        }
+    }
+
+    function mostrarAlerta(mensaje, referencia){
+        // Comprueba si ya existe una alerta        
+        const alerta = referencia.querySelector('.bg-red-600');
+        if(alerta){
+            alerta.remove()
+        }
+
+        // Generar alerta en HTML        
+        const error = document.createElement('p');
+        error.textContent = mensaje;
+        error.classList.add('bg-red-600','text-white','p-2', 'text-center')
+
+        // Inyectar el error al formulario                   
+        referencia.appendChild(error);             
+    }
+});
+
+
+
+
+
